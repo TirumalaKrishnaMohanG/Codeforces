fba6f39acd65c3ce1263a436559f1b0e8fbd56fc
TirumalaKrishnaMohanG <86065440+TirumalaKrishnaMohanG@users.noreply.github.com>
2021-10-22 11:23:40 +0530

    Update 318A.py

--- 318A.py
+++ 318A.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-
-n,k = input().split()
-n,k =int(n),(int(k)-1)
-get_RES = [x for x in range(1,int(n)+1) if x%2!=0]+[x for x in range(1,int(n)+1) if x%2==0]
-print(get_RES[k])
+n, k = map(int, input().split())
+odds, evens = (n + 1) // 2, n // 2
+if k <= odds:
+    print(int(k * 2) - 1)
+else:
+    print(int((k - odds) * 2))
